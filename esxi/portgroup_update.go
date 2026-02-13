5f59916eaf73e25a461462994054ff28cb005db6
Carlos Tronco <cars@lostroncos.org>
2026-02-13 12:26:57 -0800

    refactor(02-remove-ssh-from-portgroup): remove SSH branches from portgroup CRUD functions
    - portgroupRead now calls portgroupRead_govmomi directly
    - portgroupSecurityPolicyRead now calls portgroupSecurityPolicyRead_govmomi directly
    - portgroup create/update/delete call govmomi functions directly
    - Removed unused imports: regexp, strconv, strings, csvutil
    - Removed all SSH conditional branches

--- esxi/portgroup_update.go
+++ esxi/portgroup_update.go
@@ -31,47 +31,9 @@ func resourcePORTGROUPUpdate(d *schema.ResourceData, m interface{}) error {
 		return errors.New("Error: mac_changes must be true, false or '' to inherit")
 	}
 
-	if c.useGovmomi {
-		err := portgroupUpdate_govmomi(c, name, vlan, promiscuous_mode, forged_transmits, mac_changes)
-		if err != nil {
-			return fmt.Errorf("Failed to update portgroup: %s\n", err)
-		}
-	} else {
-		esxiConnInfo := getConnectionInfo(c)
-		var stdout string
-		var remote_cmd string
-		var err error
-		var promiscuous_mode_cmd, forged_transmits_cmd, mac_changes_cmd string
-
-		//  set vlan id
-		remote_cmd = fmt.Sprintf("esxcli network vswitch standard portgroup set -v \"%d\" -p \"%s\"",
-			vlan, name)
-
-		stdout, err = runRemoteSshCommand(esxiConnInfo, remote_cmd, "portgroup set vlan")
-		if err != nil {
-			d.SetId("") // TODO do we really want to do this? maybe only if the portgroup
-			return fmt.Errorf("Failed to set portgroup: %s\n%s\n", stdout, err)
-		}
-
-		// set the security policies.
-		if promiscuous_mode != "" {
-			promiscuous_mode_cmd = fmt.Sprintf("--allow-promiscuous=%s", promiscuous_mode)
-		}
-
-		if forged_transmits != "" {
-			forged_transmits_cmd = fmt.Sprintf("--allow-forged-transmits=%s", forged_transmits)
-		}
-
-		if mac_changes != "" {
-			mac_changes_cmd = fmt.Sprintf("--allow-mac-change=%s", mac_changes)
-		}
-
-		// There is no way to set any param to inherited, so we must use the -u to set inherited for all three params..
-		remote_cmd = fmt.Sprintf("esxcli network vswitch standard portgroup policy security set -p \"%s\" -u %s %s %s", name, promiscuous_mode_cmd, forged_transmits_cmd, mac_changes_cmd)
-		stdout, err = runRemoteSshCommand(esxiConnInfo, remote_cmd, "set the inherited portgroup security policy")
-		if err != nil {
-			return fmt.Errorf("Failed to set the inherited portgroup security policy: %s\n%s\n", stdout, err)
-		}
+	err := portgroupUpdate_govmomi(c, name, vlan, promiscuous_mode, forged_transmits, mac_changes)
+	if err != nil {
+		return fmt.Errorf("Failed to update portgroup: %s\n", err)
 	}
 
 	// Refresh
